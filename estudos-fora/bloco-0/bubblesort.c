56279f4ce89becf9a07ca760e2710c030145750b
Rangel <joaquimrangel.dev@gmail.com>
2026-08-05 14:55:33 -0300

    Bubblesort

---
+++ estudos-fora/bloco-0/bubblesort.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void bubble_sort(int v[], int n);
+
+int main(){
+    int v[10] = {5, 2, 9, 1, 5, 6,9,3,9,10},n=10;
+    bubble_sort(v,n);
+    for(int i=0;i<10;i++){
+        printf("%d-",v[i]);
+    }
+}
+
+void bubble_sort(int v[], int n){
+    for(int i=0;i<n-1;i++){
+        int trocou = 0;
+        for(int j=0;j<n-1-i;j++){
+            if(v[j]>v[j+1]){
+                int temp = v[j];
+                v[j]=v[j+1];
+                v[j+1]=temp;
+                trocou = 1;
+            }
+        }
+        if(trocou == 0) break;
+    }
+}
