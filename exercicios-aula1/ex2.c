2f377036c7e2f47950a4b5e3a3de3d9193f713c6
Rangel <joaquimrangel.dev@gmail.com>
2026-08-10 16:30:52 -0300

    exercicio simples

---
+++ exercicios-aula1/ex2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int v[50];
+    for(int i=0;i<50;i++){
+        v[i]=(i+5*i)%(i+1);
+    }
+    for(int i=0;i<50;i++){
+        printf("%d-",v[i]);
+    }
+    return 0;
+}
