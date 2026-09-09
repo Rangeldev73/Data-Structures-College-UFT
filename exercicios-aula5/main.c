37ae54a71b035a5ab2766ea493f5e0fc248431c7
Rangel <joaquimrangel.dev@gmail.com>
2026-09-09 11:05:20 -0300

    class 5

---
+++ exercicios-aula5/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "stack/stack.h"
+#define MAX 10
+
+int main(){
+
+    Stack* principal = Stack_create();
+    Stack* secundaria = Stack_create();
+
+    char comando;
+    int id;
+
+    while(scanf(" %c %d",&comando,&id)==2){
+        if(comando=='0'&&id==0){
+            break;
+        }
+
+        if(comando=='i'){
+            if(getSize(principal)==MAX){
+                printf("overflow\n");
+            }
+            else{
+                Stack_push(principal, id);
+            }
+        }
+        else if(comando=='r'){
+
+            bool encontrado=false;
+            int catual;
+            while(!Stack_empty(principal)){
+                Stack_pop(principal,&catual);
+                printf("%d, ", catual);
+                if(catual==id){
+                    encontrado=true;
+                    break;
+                }
+                else {
+                    Stack_push(secundaria, catual);
+
+                }
+            }
+            if(!encontrado){
+                printf("underflow\n");
+            }
+            else{
+                printf("\n");
+            }
+
+            while(!Stack_empty(secundaria)){
+                Stack_pop(secundaria,&catual);
+                Stack_push(principal, catual);
+            }
+        }
+    }
+    Stack_destroy(principal);
+    Stack_destroy(secundaria);
+    return 0;
+}
